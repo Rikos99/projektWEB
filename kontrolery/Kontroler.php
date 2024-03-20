abf293cf61079c6adbe4b155661f3c9a880db0fe
Richard Dluhoš <rikosak99@gmail.com>
2024-03-20 10:36:14 +0100

    Opraven bug u stranky Chyba

--- kontrolery/Kontroler.php
+++ kontrolery/Kontroler.php
@@ -1,12 +1,12 @@
 <?php
 abstract class Kontroler {
     protected $pohled = ""; // název souboru s pohledem (bez přípony .phtml)
-
     protected $data = []; //pole pro data
 
     abstract public function zpracuj($parametry);
 
     public function vypisPohled() {
+        extract($this->data);
         require "pohledy/{$this->pohled}.phtml";
     } 
 
