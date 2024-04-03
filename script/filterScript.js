cbeffa9ccec82a0a37a11251dedf418f5f13edf9
Lvonka <134289556+Lvonka@users.noreply.github.com>
2024-04-03 19:45:03 +0200

    mrdkovina to je sračka zmrdství zabiju se

--- script/filterScript.js
+++ script/filterScript.js
@@ -8,3 +8,10 @@ $(document).mouseup(function(e) {
         $(".dropdown-content1").removeClass("show");
     }
 });
+
+$(document).mouseup(function(e) {
+    var container = $(".dropdown1");
+    if (!container.is(e.target) && container.has(e.target).length === 0 && !$(e.target).hasClass('dropbtn1')) {
+        $(".dropdown-content1").removeClass("show");
+    }
+});
