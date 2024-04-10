76b0df945243f656cbce9cfff282868ec694ca59
Lvonka <134289556+Lvonka@users.noreply.github.com>
2024-04-10 10:35:51 +0200

    asdasd

--- script/filterScript.js
+++ script/filterScript.js
@@ -1,17 +1,8 @@
-function toggleDropdown() {
-    $(".dropdown-content1").toggleClass("show");
-}
+$(".dropbtn1").on("click",function(){
 
-$(document).mouseup(function(e) {
-    var container = $(".dropdown1");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-        $(".dropdown-content1").removeClass("show");
-    }
-});
+    $(this).next().toggleClass("show");
+    
+}) 
+    
 
-$(document).mouseup(function(e) {
-    var container = $(".dropdown1");
-    if (!container.is(e.target) && container.has(e.target).length === 0 && !$(e.target).hasClass('dropbtn1')) {
-        $(".dropdown-content1").removeClass("show");
-    }
-});
+
