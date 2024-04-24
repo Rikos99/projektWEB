65c674f6c5cb22484aaaa0d5cfd4f4ac2b3cf0d5
Richard Dluhoš <rikosak99@gmail.com>
2024-04-24 09:52:07 +0200

    oprava css

--- kontrolery/Kontroler.php
+++ kontrolery/Kontroler.php
@@ -3,7 +3,7 @@ abstract class Kontroler {
     protected $pohled = ""; // název souboru s pohledem (bez přípony .phtml)
     protected array $data = []; //pole pro data
     protected $prihlasenyUzivatel;
-    protected array $cssCesty = ["style.css", "navstyle.css", "maintextstyle.css", "sidemainstyle.css"];
+    protected array $cssCesty = [];
 
     public function __construct()
     {
