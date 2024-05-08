2ffc09c90960eabb6a73d10f5d91921eb5bf762e
smhshityourpants <159340905+smhshityourpants@users.noreply.github.com>
2024-05-09 00:46:54 +0200

    Add files via upload

--- kontrolery/ProfilKontroler.php
+++ kontrolery/ProfilKontroler.php
@@ -1,22 +1,57 @@
 <?php
 
+require_once("modely/Db.php");
+require_once("modely/ProfilModel.php");
+require_once("modely/config.php");
+
 class ProfilKontroler extends Kontroler {
-    public function zpracuj($parametry){
+    private $profilModel;
+    protected $prihlasenyUzivatel;
 
-        $this->cssCesty=["profilstyle.css"];
-
-        $profilModel = new ProfilModel();
-        
-        
-        if(!empty($_POST)){
-            $profilModel->aktualizovatprofil();   
-             
-        }
-        $this->pohled = "profil";
-        $this->data["title"] = "Profil";
-        $this->data["infoProfilu"] = $profilModel->ziskatInfoUzivatele($_SESSION["uzivatel"]["Id"]);
-
+    public function __construct() {
+        parent::__construct();
+        $this->profilModel = new ProfilModel(Db::pripoj(DB_SERVER, DB_UZIVATEL, DB_HESLO, DB_NAME));
+        $this->prihlasenyUzivatel = $this->vratPrihlasenehoUzivatele();
     }
 
+    public function zpracuj($parametry){
+        if(isset($_POST["ulozit"])){
+            $jmeno = isset($_POST["jmeno"]) ? $_POST["jmeno"] : "";
+            $prijmeni = isset($_POST["prijmeni"]) ? $_POST["prijmeni"] : "";
+            $trida = isset($_POST["trida"]) ? $_POST["trida"] : "";
+            $prezdivka = isset($_POST["prezdivka"]) ? $_POST["prezdivka"] : ""; // Přidána kontrola na existenci prezdivky
     
+    
+            // Aktualizace údajů v databázi
+            $this->profilModel->aktualizovatProfil($jmeno, $prijmeni, $prezdivka, $trida); // Předávání správných argumentů
+            $_SESSION["zpravy"]["uspech"] = "Údaje byly úspěšně aktualizovány.";
+        } elseif(isset($_POST["ulozit_heslo"])) {
+            $heslo = isset($_POST["heslo"]) ? $_POST["heslo"] : "";
+            $heslo2 = isset($_POST["heslo2"]) ? $_POST["heslo2"] : "";
+
+
+            // Aktualizace hesla 
+            if($heslo === $heslo2) {
+                $this->profilModel->zmenitHeslo($heslo, $this->prihlasenyUzivatel['Id']);
+                $_SESSION["zpravy"]["uspech"] = "Heslo bylo úspěšně změněno.";
+            } else {
+                $_SESSION["zpravy"]["chyba"] = "Hesla se neshodují.";
+            }
+        }
+ 
+        $data["infoProfilu"] = $this->profilModel->ziskatInfoUzivatele($this->prihlasenyUzivatel['Id']);
+    
+        $this->pohled = "profil";
+        $this->data = $data;
+    }
+
+    protected function vratPrihlasenehoUzivatele() {
+        if (isset($_SESSION["uzivatel"])) {
+            return $_SESSION["uzivatel"];
+        } else {
+            return false;
+        }
+    }
 }
+
+?>
--- kontrolery/SmerovacKontroler.php
+++ kontrolery/SmerovacKontroler.php
@@ -1,58 +1,58 @@
 <?php
 class SmerovacKontroler extends Kontroler {
-  protected $kontroler; // objekt kontroleru
-  
-  // podle URL ($parametry[0]) "předané z indexu" 
-    // nalezne správný kontroler a předá mu řízení 
-  public function zpracuj($parametry) {
-     $url = $parametry[0];
-     $castiCesty = $this->parsujURL($url);
-     if (empty($castiCesty[0])) {
-        // přesměrujeme na výchozí kontroler
-        $this->presmeruj("uvod");
-     }
-     else { // v URL je neprázdná cesta (tedy určen kontroler)      
-          // ["uzivatel", "10", "editace"]    // $castiCesty
-          $castNazvuKontroleru = array_shift($castiCesty); // vrátí první prvek a ostatní prvky posune na začátek
-          $nazevKontroleru = $this->pomlckyDoVelbloudiNotace($castNazvuKontroleru)
-                              . "Kontroler";
+    protected $kontroler; // objekt kontroleru
 
-          if (file_exists("kontrolery/$nazevKontroleru.php")) {
+    // Metoda zpracuj podle URL předané z indexu nalezne správný kontroler a předá mu řízení 
+    public function zpracuj($parametry) {
+        require_once("modely/Db.php"); 
+        require_once("modely/ProfilModel.php");
 
-            $this->kontroler = new $nazevKontroleru;
-            $this->kontroler->zpracuj($castiCesty);
+        $url = $parametry[0];
+        $castiCesty = $this->parsujURL($url);
+        if (empty($castiCesty[0])) {
+            // přesměrujeme na výchozí kontroler
+            $this->presmeruj("uvod");
+        } else { // v URL je neprázdná cesta (tedy určen kontroler)      
+            // ["uzivatel", "10", "editace"]    // $castiCesty
+            $castNazvuKontroleru = array_shift($castiCesty); // vrátí první prvek a ostatní prvky posune na začátek
+            $nazevKontroleru = $this->pomlckyDoVelbloudiNotace($castNazvuKontroleru)
+                                . "Kontroler";
 
-            $this->pohled = "rozlozeni";
+            if (file_exists("kontrolery/$nazevKontroleru.php")) {
 
-          } else { // třída kontroleru neexistuje
-            // přesměrujeme na chybový kontroler
-            $this->presmeruj("chyba/$castNazvuKontroleru");
-          }                             
-     } 
-  }
-  
-  // z editace-studenta udělá EditaceStudenta
-  private function pomlckyDoVelbloudiNotace($text) {
-      $text = str_replace("-", " ", $text);
-      $text = ucwords($text);
-      $text = str_replace(" ", "", $text);
-      return $text;
-  }
-  
-  // z https://localhost/uzivatel/10/editace 
-  // udělá pole
-  // ["uzivatel", "10", "editace"]
-  private function parsujURL($url) {
-      $naparsovanaURL = parse_url($url);
-      $cesta = $naparsovanaURL["path"];
-      
-      $cesta = ltrim($cesta, "/"); // odebere počáteční lomítko
-      $cesta = trim($cesta); // odebere bílé znaky
-      
-      $rozdelenaCesta = explode("/", $cesta);
-      
-      return $rozdelenaCesta;
-  }
+                require_once("kontrolery/$nazevKontroleru.php"); // Upravená cesta
+
+                $this->kontroler = new $nazevKontroleru;
+                $this->kontroler->zpracuj($castiCesty);
+
+                $this->pohled = "rozlozeni";
+
+            } else { // třída kontroleru neexistuje
+                // přesměrujeme na chybový kontroler
+                $this->presmeruj("chyba/$castNazvuKontroleru");
+            }                             
+        } 
+    }
+
+    // Metoda převede pomlčkovou notaci na velbloudí notaci
+    private function pomlckyDoVelbloudiNotace($text) {
+        $text = str_replace("-", " ", $text);
+        $text = ucwords($text);
+        $text = str_replace(" ", "", $text);
+        return $text;
+    }
+
+    // Metoda parsujURL rozdělí URL na jednotlivé části
+    private function parsujURL($url) {
+        $naparsovanaURL = parse_url($url);
+        $cesta = $naparsovanaURL["path"];
+    
+        $cesta = ltrim($cesta, "/"); // odebere počáteční lomítko
+        $cesta = trim($cesta); // odebere bílé znaky
+    
+        $rozdelenaCesta = explode("/", $cesta);
+    
+        return $rozdelenaCesta;
+    }
 }
-
 ?>
